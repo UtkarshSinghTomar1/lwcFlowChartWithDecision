f3b5f69eb04aeae28958e641540b89db3c87d2dd
UtkarshSinghTomar1 <136987642+UtkarshSinghTomar1@users.noreply.github.com>
2023-06-26 20:50:37 +0530

    Add files via upload

---
+++ cdCmpStageMasterFlowVertical2.js
@@ -0,0 +1,185 @@
+import { LightningElement,api } from 'lwc';
+import { loadScript,loadStyle } from 'lightning/platformResourceLoader';
+import flowmincss from '@salesforce/resourceUrl/flowmincss';
+import getBusinessrocessAndStage3 from '@salesforce/apex/CDBusinessProcessWithStageController.getBusinessrocessAndStage3';
+import jquery from '@salesforce/resourceUrl/jquery';
+export default class CdCmpStageMasterFlowTest5 extends LightningElement {
+
+
+    initialized = false;
+        @api  recordId;
+        markupArray =  [""];
+        dataMap = new Map();
+    renderedCallback() {
+        if (this.initialized) return;
+        this.initialized = true;
+
+        Promise.all([
+               loadScript(this, jquery),
+               loadStyle(this, flowmincss)
+            ])
+                .then(response=> {
+                    this.intializeData();
+                })
+                .catch((error) => {
+                    this.error = error;
+                });
+    }
+
+    intializeData(){
+           getBusinessrocessAndStage3({recordId : this.recordId })
+           .then(result => {
+            if(result != undefined && result != null){
+             const dataFromApex = JSON.parse(result);
+             console.log('dataFromApex',dataFromApex);
+             if(dataFromApex.dataLst != undefined && dataFromApex.dataLst != null ){
+                for (let i = 0; i < dataFromApex.dataLst.length; i++) {
+                 this.dataMap.set(dataFromApex.dataLst[i].sfdcId,dataFromApex.dataLst[i]);
+               }
+             }
+             console.log('dataMap',this.dataMap);
+             if(dataFromApex != undefined && dataFromApex != null ){
+
+             var items = dataFromApex.childRecords;
+             var firstStage = dataFromApex.parentRecord;
+             var data1 = this.unflatten(items);
+              //Child Object
+              const child = {
+              name: firstStage.name,
+              sfdcId:firstStage.sfdcId,
+              Id:firstStage.Id,
+              stage:firstStage.stage,
+              children: data1,
+              };
+              //Assign child to parent
+             const data = {
+               parent: child, // property name may be an identifier
+              };
+             console.log('data from vs',JSON.stringify(data));
+
+             this.createList(data);
+             this.markupArray.push("</ol>");
+             const container = this.template.querySelector('.container');
+             const CONTAINER_HTML = "<div class='flow-decision-tree' style='padding-left:10%'>"+this.markupArray.join("")+"</div>";
+             const regex = /<([^>]+)\s*>\s*<\/\1\s*>/g;
+             const finalCONTAINER_HTML = CONTAINER_HTML.replaceAll(regex,'');
+             console.log(finalCONTAINER_HTML); //This is html file generated dynamically using javascript
+             container.innerHTML= finalCONTAINER_HTML;
+             }
+            }
+           })
+           .catch(error => {
+                 console.log('error',error);
+           });
+           };
+
+
+               string_between_strings(startStr, endStr, str) {
+                   let pos = str.indexOf(startStr) + startStr.length;
+                   return str.substring(pos, str.indexOf(endStr, pos));
+               }
+
+
+
+               createList = (items) => {
+              console.log('items',items);
+                switch ($.type(items)) {
+                   case "object":
+                     this.getItems(items);
+                     break;
+
+                               }
+               };
+
+                   // get items in the object
+               getItems = (items) => {
+
+                           debugger;
+                                 for (const item in items) {
+                                   this.markupArray.push(`<li>`);
+                                   // fetch the parent object
+                                   let details = items[item];
+                                   this.getDetails(details);
+                                   // push the closing tag for parent
+                                   this.markupArray.push("</li>");
+                                 }
+                           };
+
+
+
+
+               unflatten(items) {
+                 var tree = [],
+                 mappedArr = {}
+                  items.forEach(function(item) {
+                   var id = item.Id;
+                   if (!mappedArr.hasOwnProperty(id)) { // in case of duplicates
+                     mappedArr[id] = item; // the extracted id as key, and the item as value
+                     mappedArr[id].children = [];  // under each item, add a key "children" with an empty array as value
+                   }
+                   })
+
+                 // Loop over hash table
+                 for (var id in mappedArr) {
+                   if (mappedArr.hasOwnProperty(id)) {
+                     var mappedElem = mappedArr[id];
+                   if (mappedElem.Parent) {
+                       var parentId = mappedElem.Parent;
+                       const obj1  = {
+                         child: mappedElem, // property name may be an identifier
+                        };
+                       mappedArr[parentId].children.push(obj1);
+                     }// If the element is at the root level, directly push to the tree
+                     else {
+                         const obj  = {
+                           child: mappedElem, // property name may be an identifier
+                          };
+                       tree.push(obj);
+                     }
+                   }
+                 }
+
+                 return tree;
+               }
+
+
+
+                   // get details
+                                   getDetails = (details) => {
+                                       debugger;
+                                     // iterate over the detail items of object
+                                     for (const detail in details) {
+                                       // fetch the value of each item
+                                       if (detail == "children" && details[detail] != undefined && details[detail] != null) {
+                                         this.markupArray.push("<ul>");
+                                         details[detail].forEach((element) => {
+                                           this.getItems(element);
+                                         });
+
+                                         this.markupArray.push("</ul>");
+                                       } else {
+                                           if(detail == "name"){
+                                            const substr1 = this.string_between_strings('href="/', '">',details[detail]);
+                                            console.log(substr1);
+                                            console.log(this.dataMap.get(substr1));
+                                            if(this.dataMap.get(substr1).stage == 'start'){
+                                               this.markupArray.push(`<span style='background-color: #DCDCDC;outline:solid 1px;' class='terminator start'> ${details[detail]}</span>`);
+                                            }
+                                            else if(this.dataMap.get(substr1).stage == 'Decision'){
+                                               this.markupArray.push(`<div class="poligon"><span style='background-color: #FFFACD;outline:solid 1px;' class='decision'> ${details[detail]} </span></div>`);
+                                            }
+                                            else if(this.dataMap.get(substr1).stage == 'end'){
+                                                this.markupArray.push(`<span style='background-color: #DCDCDC;outline:solid 1px;' class='terminator end'> ${details[detail]}</span>`);
+                                             }
+                                            else{
+                                              this.markupArray.push(`<span style='background-color: #f1f4fa;outline:solid 1px;' class='process'> ${details[detail]} </span>`);
+                                            }
+
+
+
+                                         }
+                                       }
+                                     }
+                                   }
+
+           }
